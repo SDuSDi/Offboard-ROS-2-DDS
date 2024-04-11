cda5aae180235b53839084f46224c5eee3301c34
SDuSDi <bmartinez.ext@catec.aero>
2024-04-11 10:40:16 +0200

    Third push FIXED
    Added code to offb_node
    Added config file for vscode

---
+++ workspace/build/drone/CMakeFiles/node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node.dir/src/offb_nope.cpp.o"
+  "node"
+  "node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
